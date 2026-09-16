2835f94c31591fbbd2669c616b719056d5306038
Elouan Martinet <exa@elou.world>
2026-09-16 10:20:36 +0200

    Refactor weapon and class shortcuts

--- chat-shortcuts/chat-shortcuts.lua
+++ chat-shortcuts/chat-shortcuts.lua
@@ -29,6 +29,73 @@ local version = "1.0"
 local ENTITYNUM_NONE = (1 << 10) - 1
 local ENTITYNUM_WORLD = (1 << 10) - 2
 
+local et_weapons = {
+	-- [et.WP_NONE] = "", -- 0
+	[et.WP_KNIFE] = "Knife", -- 1
+	[et.WP_LUGER] = "Luger 9mm", -- 2 (Luger)
+	[et.WP_MP40] = "MP 40", -- 3
+	[et.WP_GRENADE_LAUNCHER] = "Grenade", -- 4
+	[et.WP_PANZERFAUST] = "Panzerfaust", -- 5
+	[et.WP_FLAMETHROWER] = "Flamethrower", -- 6
+	[et.WP_COLT] = ".45ACP 1911", -- 7 (Colt)
+	[et.WP_THOMPSON] = "Thompson", -- 8
+	[et.WP_GRENADE_PINEAPPLE] = "Grenade", -- 9 (Pineapple)
+	[et.WP_STEN] = "Sten", -- 10 (Sten gun)
+	[et.WP_MEDIC_SYRINGE] = "Syringe", -- 11
+	[et.WP_AMMO] = "Ammo Pack", -- 12
+	[et.WP_ARTY] = "Artillery", -- 13
+	[et.WP_SILENCER] = "Luger 9mm", -- 14 (Silenced Luger)
+	[et.WP_DYNAMITE] = "Dynamite", -- 15 (Dynamite Weapon)
+	-- [et.WP_SMOKETRAIL] = "", -- 16
+	-- [et.WP_MAPMORTAR] = "", -- 17
+	-- [et.VERYBIGEXPLOSION] = "", -- 18
+	[et.WP_MEDKIT] = "Medic Pack", -- 19
+	[et.WP_BINOCULARS] = "Binoculars", -- 20
+	[et.WP_PLIERS] = "Pliers", -- 21 (Special)
+	[et.WP_SMOKE_MARKER] = "Airstrike Marker", -- 22 (smokeGrenade)
+	[et.WP_KAR98] = "K43", -- 23 (K43 Rifle)
+	[et.WP_CARBINE] = "Garand", -- 24 (M1 Garand)
+	[et.WP_GARAND] = "Garand", -- 25 (Scoped M1 Garand)
+	[et.WP_LANDMINE] = "Landmine", -- 26
+	[et.WP_SATCHEL] = "Satchel Charge", -- 27
+	[et.WP_SATCHEL_DET] = "Satchel Charge Detonator", -- 28
+	[et.WP_SMOKE_BOMB] = "Smoke Bomb", -- 29
+	[et.WP_MOBILE_MG42] = "Mobile MG 42", -- 30
+	[et.WP_K43] = "K43", -- 31 (Scoped K43 Rifle)
+	[et.WP_FG42] = "FG 42", -- 32 (FG 42 Paratroop Rifle)
+	[et.WP_DUMMY_MG42] = "MG", -- 33 (BLANK)
+	[et.WP_MORTAR] = "Mortar", -- 34
+	[et.WP_AKIMBO_COLT] = "Akimbo .45ACP 1911s", -- 35 (Akimbo Colt)
+	[et.WP_AKIMBO_LUGER] = "Akimbo Luger 9mms", -- 36 (Akimbo Luger)
+	[et.WP_GPG40] = "Rifle Grenade", -- 37 (GPG40)
+	[et.WP_M7] = "Rifle Grenade", -- 38 (M7)
+	[et.WP_SILENCED_COLT] = ".45ACP 1911", -- 39 (Silenced Colt)
+	[et.WP_GARAND_SCOPE] = "Garand", -- 40 (M1 Garand Scope)
+	[et.WP_K43_SCOPE] = "K43", -- 41 (K43 Rifle Scope)
+	[et.WP_FG42_SCOPE] = "FG 42", -- 42 (FG 42 Paratroop Rifle Scope)
+	[et.WP_MORTAR_SET] = "Mortar", -- 43 (Mounted Mortar)
+	[et.WP_MEDIC_ADRENALINE] = "Adrenaline Syringe", -- 44
+	[et.WP_AKIMBO_SILENCEDCOLT] = "Akimbo .45ACP 1911s", -- 45 (Silenced Akimbo Colt)
+	[et.WP_AKIMBO_SILENCEDLUGER] = "Akimbo Luger 9mms", -- 46 (Silenced Akimbo Luger)
+	[et.WP_MOBILE_MG42_SET] = "Mobile MG 42 Bipod", -- 47
+	[et.WP_KNIFE_KABAR] = "Ka-Bar", -- 48
+	[et.WP_MOBILE_BROWNING] = "Mobile Browning", -- 49
+	[et.WP_MOBILE_BROWNING_SET] = "Mobile Browning Bipod", -- 50
+	[et.WP_MORTAR2] = "Granatwerfer", -- 51
+	[et.WP_MORTAR2_SET] = "Granatwerfer", -- 52 (Mounted Granatwerfer)
+	[et.WP_BAZOOKA] = "Bazooka", -- 53
+	[et.WP_MP34] = "MP 34", -- 54 (MP34)
+	[et.WP_AIRSTRIKE] = "Airstrike", -- 55
+}
+
+local et_classes = {
+	[0] = "Soldier",
+	[1] = "Medic",
+	[2] = "Engineer",
+	[3] = "Field Ops",
+	[4] = "Covert Ops",
+}
+
 local last_killer = {}
 
 function formatUsername(clientNum)
@@ -80,107 +147,10 @@ local shortcuts = {
 	end,
 	["w"] = function (sender, receiver) -- name of current weapon
 		local weapon = et.gentity_get(sender, "ps.weapon")
-		if weapon == 1 then
-			return "Knife"
-		elseif weapon == 2 then
-			return "Luger"
-		elseif weapon == 3 then
-			return "MP 40"
-		elseif weapon == 4 then
-			return "Grenade"
-		elseif weapon == 5 then
-			return "Panzerfaust"
-		elseif weapon == 6 then
-			return "Flamethrower"
-		elseif weapon == 7 then
-			return "Colt"
-		elseif weapon == 8 then
-			return "Thompson"
-		elseif weapon == 9 then
-			return "Pineapple"
-		elseif weapon == 10 then
-			return "Sten gun"
-		elseif weapon == 11 then
-			return "Syringe"
-		elseif weapon == 12 then
-			return "Ammo Pack"
-		elseif weapon == 13 then
-			return "Artillery"
-		elseif weapon == 14 then
-			return "Silenced Luger"
-		elseif weapon == 15 then
-			return "Dynamite Weapon"
-		elseif weapon == 20 then
-			return "Binoculars"
-		elseif weapon == 21 then
-			return "Special"
-		elseif weapon == 23 then
-			return "K43 Rifle"
-		elseif weapon == 24 then
-			return "M1 Garand"
-		elseif weapon == 25 then
-			return "Scoped M1 Garand"
-		elseif weapon == 26 then
-			return "Landmine"
-		elseif weapon == 27 then
-			return "Satchel Charge"
-		elseif weapon == 28 then
-			return "Satchel Charge Detonator"
-		elseif weapon == 29 then
-			return "Smoke Bomb"
-		elseif weapon == 30 then
-			return "Mobile MG 42"
-		elseif weapon == 31 then
-			return "Scoped K43 Rifle"
-		elseif weapon == 32 then
-			return "FG 42 Paratroop Rifle"
-		elseif weapon == 34 then
-			return "Mortar"
-		elseif weapon == 35 then
-			return "Akimbo Colt"
-		elseif weapon == 36 then
-			return "Akimbo Luger"
-		elseif weapon == 37 then
-			return "GPG40"
-		elseif weapon == 38 then
-			return "M7"
-		elseif weapon == 39 then
-			return "Silenced Colt"
-		elseif weapon == 40 then
-			return "M1 Garand Scope"
-		elseif weapon == 41 then
-			return "K43 Rifle Scope"
-		elseif weapon == 42 then
-			return "FG 42 Paratroop Rifle Scope"
-		elseif weapon == 43 then
-			return "Mounted Mortar"
-		elseif weapon == 44 then
-			return "Adrenaline Syringe"
-		elseif weapon == 45 then
-			return "Silenced Akimbo Colt"
-		elseif weapon == 46 then
-			return "Silenced Akimbo Luger"
-		elseif weapon == 47 then
-			return "Mobile MG 42 Bipod"
-		elseif weapon == 48 then
-			return "Ka-Bar"
-		elseif weapon == 49 then
-			return "Mobile Browning"
-		elseif weapon == 50 then
-			return "Mobile Browning Bipod"
-		elseif weapon == 51 then
-			return "Granatwerfer"
-		elseif weapon == 52 then
-			return "Mounted Granatwerfer"
-		elseif weapon == 53 then
-			return "Bazooka"
-		elseif weapon == 54 then
-			return "MP34"
-		elseif weapon == 55 then
-			return "Airstrike"
-		else
-			return "Nothing"
+		if et_weapons[weapon] ~= nil then
+			return et_weapons[weapon]
 		end
+		return "weapon"
 	end,
 	["t"] = function (sender, receiver) -- ammo for current weapon
 		local weapon = et.gentity_get(sender, "ps.weapon")
@@ -190,17 +160,7 @@ local shortcuts = {
 	end,
 	["c"] = function (sender, receiver) -- name of current class
 		local class = et.gentity_get(sender, "sess.playerType")
-		if class == 1 then
-			return "Medic"
-		elseif class == 2 then
-			return "Engineer"
-		elseif class == 3 then
-			return "Field Ops"
-		elseif class == 4 then
-			return "Covert Ops"
-		else
-			return "Soldier"
-		end
+		return et_classes[class]
 	end,
 	["o"] = function (sender, receiver) -- other player receiving the message
 		return formatUsername(receiver)
